b447e8faee83fee2abd845aca5936b95773aa923
Sara Cook <saracook087@gmail.com>
2022-08-15 09:09:25 +0100

    added Map and routes

--- src/Components/Itinerary.js
+++ src/Components/Itinerary.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Map from "./Map";
+import {
+  useLoadScript,
+} from "@react-google-maps/api";
 
 const Itinerary = () => {
-  return (
-    <h1>Itinerary</h1>
-  )
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return (<div><h1>Loading...</h1></div>)
+  } else {
+    return (<div><Map /></div>)
+  }
 };
 
 export default Itinerary;
